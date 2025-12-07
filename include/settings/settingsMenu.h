1c9ba273c6066d1c4cf3c180d54c5066be151ce0
link2war <link2war.riders@gmail.com>
2025-12-07 18:23:59 +0100

    futur classes

---
+++ include/settings/settingsMenu.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <BBOP/Graphics.h>
+
+
+class SettingsMenu
+{
+    private :
+        Sprite * background;
+        Sprite * Retour;
+
+    public :
+        SettingsMenu();
+        ~SettingsMenu();
+
+        void update();
+        void Draw();
+};
---
+++ src/settings/settingsMenu.cpp
@@ -0,0 +1,21 @@
+#include "../../include/settings/settingsMenu.h"
+
+SettingsMenu::SettingsMenu()
+{
+    std::cout << "SettingsMenu initialisé" << std::endl;
+}
+
+SettingsMenu::~SettingsMenu()
+{
+    std::cout << "SettingsMenu supprimé" <<std::endl;
+}
+
+void SettingsMenu::update()
+{
+
+}
+
+void SettingsMenu::Draw()
+{
+    
+}
